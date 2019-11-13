c78d0a0cee382ff6a8964265b1b72aa8edbc6019
Administración IMJ <laura.ortiz2522013@gmail.com>
2019-11-13 15:22:28 -0600

    Updates

---
+++ precache-manifest.e2fff088c25f22225071e6ada2d688f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cd38218393c4b7e8ee688b21993772",
+    "url": "/DesignMinds/index.html"
+  },
+  {
+    "revision": "8832065d8416ba24426b",
+    "url": "/DesignMinds/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0cda72a6e973eaf19c1c",
+    "url": "/DesignMinds/static/js/2.5d8e8df6.chunk.js"
+  },
+  {
+    "revision": "8832065d8416ba24426b",
+    "url": "/DesignMinds/static/js/main.e16c7023.chunk.js"
+  },
+  {
+    "revision": "e54c590a8d47f5cca5e8",
+    "url": "/DesignMinds/static/js/runtime-main.cebd32d8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DesignMinds/static/media/logo.25bf045c.svg"
+  }
+]);
